badf34a65d28fceb0ce2d53ef3f9567c1635e5a4
castel <aitorcastel@gmail.com>
2023-07-04 13:27:40 +0100

    phpfilter_lfitorce 1

---
+++ phpfilter_lfitorce/demo-web/index.php
@@ -0,0 +1,5 @@
+<h2>vuln page</h2>
+<p style="text-align:center;"><img src="img" width="200" height="150" alt="fine"></p>
+<p>Stuff:</p>
+<?php include($_GET['page']);?>
+<p>--- End of index.php ----</p>
